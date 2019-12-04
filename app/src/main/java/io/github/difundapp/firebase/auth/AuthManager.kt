2cdf58259222eeebe6913109225e95cafe8180b8
n0g4y0 <n0g4y0@gmail.com>
2019-12-04 12:10:29 -0400

    añadiendo las librerias

---
+++ app/src/main/java/io/github/difundapp/firebase/auth/AuthManager.kt
@@ -0,0 +1,17 @@
+package io.github.difundapp.firebase.auth
+
+import android.app.Activity
+import android.content.Context
+import com.firebase.ui.auth.AuthUI
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
+
+const val RC_SIGN_IN = 1000
+
+class AuthManager {
+
+
+
+
+}
